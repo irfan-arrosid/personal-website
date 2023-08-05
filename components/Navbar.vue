29049b5833d150c0e44d4d45598872e3be0d0e92
irfan-arrosid <irfanarrosid4@gmail.com>
2023-08-05 13:07:54 +0700

    Style: Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="flex items-center" style="height: 70px">
+        <div style="height: 48px" class="pr-5">
+            <img src="/favicon.ico" alt="logo" class="h-full">
+        </div>
+        <ul class="text-white text-base flex items-center ml-auto">
+            <li>
+                <nuxt-link class="px-4 py-3" to="#">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="px-4 py-3" to="#">About</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="px-4 py-3" to="#">Projects</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="px-4 py-3" to="#">Blog</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link class="px-4 py-3" to="#">Contact</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="text-lg font-bold text-center">
-        <h1>Hello World!</h1>
+    <div class="bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900">
+        <div class="container mx-auto relative">
+            <Navbar />
+        </div>
     </div>
 </template>
