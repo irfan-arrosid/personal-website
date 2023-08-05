3e96c5b61e75cbb3bf4cb6c67384bd7710b29f28
irfan-arrosid <irfanarrosid4@gmail.com>
2023-08-05 23:02:57 +0700

    Style: Hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900">
+    <section class="bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-700 via-blue-800 to-gray-900">
         <div class="container mx-auto relative">
             <Navbar />
         </div>
-    </div>
+        <div class="container mx-auto relative">
+            <div class="flex flex-col justify-center" style="height: 666px">
+                <h1 class="text-white font-bold text-7xl tracking-wide w-full py-1">Irfan Arrosid</h1>
+                <p class="text-white text-2xl w-full py-1">Full-Stack Developer from Indonesia</p>
+                <div class="flex flex-wrap gap-6 mt-8">
+                    <a class="bg-white px-5 py-2 rounded-lg" href="#">LinkedIn</a>
+                    <a class="border-solid border-2 border-white text-white px-5 py-2 rounded-lg" href="#">About</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
